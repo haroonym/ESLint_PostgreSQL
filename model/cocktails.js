7edb7b013889ea3c42dfca150c904690ddc32adb
unknown <younas.h03@htlwienwest.at>
2021-05-19 11:18:44 +0200

    5.Aufgabe => Route, die die Zutaten liefert

--- model/cocktails.js
+++ model/cocktails.js
@@ -8,6 +8,26 @@ async function getCocktails() {
   };
 }
 
-module.exports = {
-    getCocktails,
+async function getZutaten(cname) {
+  const {
+    rows,
+  } = await db.query(
+    'SELECT zbez FROM cocktail JOIN besteht b on cocktail.cid = b.cid JOIN zutat z on b.zid = z.zid WHERE cname = $1',
+    [cname],
+  );
+  if (rows.length > 0)
+    return {
+      code: 200,
+      data: rows,
+    };
+  else
+    return {
+      code: 404,
+      data: `the specified cocktail with the Name ${cname} was not found in the database`,
+    };
 }
+
+module.exports = {
+  getCocktails,
+  getZutaten,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 const router = express.Router();
-const { getCocktails } = require('../model/cocktails');
+const { getCocktails, getZutaten } = require('../model/cocktails');
 
 router.get(
   '/cocktails',
   asyncHandler(async (req, res) => {
     const result = await getCocktails();
+    res.status(result.code).json(result);
+  }),
+);
+
+router.get(
+  '/cocktails/:cname/zutaten',
+  asyncHandler(async (req, res) => {
+    const result = await getZutaten(req.params.cname);
     res.status(result.code).json(result);
   }),
 );
